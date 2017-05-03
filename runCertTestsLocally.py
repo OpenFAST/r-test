a8d08e200c3d493f763b2108f4baee14293c509e
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-03 13:57:44 -0600

    add option to pass an openfast executable as input argument

--- runCertTestsLocally.py
+++ runCertTestsLocally.py
@@ -1,12 +1,55 @@
 """
     This script runs all of the CertTest cases to create a local 'gold standard'
     set of solutions.
+
+    Usage: python runCertTestsLocally.py openfast_executable
+     - openfast_executable is an optional argument pointing to the openfast executable of choice.
+     - if not openfast_executable is given, an attempt will be made to search for one in $PATH
+
+    Examples:
+    $ python runCertTestsLocally.py
+    $ python runCertTestsLocally.py openfast
+    $ python runCertTestsLocally.py /openfast/install/bin/openfast
 """
 
 import os
 import sys
 import shutil
 import subprocess
+
+def exitWithError(error):
+    print error
+    sys.exit(1)
+
+# if no openfast executable was given, search in path
+if len(sys.argv) == 1:
+    try:
+        devnull = open(os.devnull, 'w')
+        subprocess.call("openfast", stdout=devnull)
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            exitWithError("{}: openfast".format(e))
+        else:
+            raise
+    else:
+        print "Using openfast executable found in path"
+
+# verify that the given executable exists and can be run
+elif len(sys.argv) == 2:
+    executable = sys.argv[1]
+    try:
+        devnull = open(os.devnull, 'w')
+        subprocess.call(executable, stdout=devnull)
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            exitWithError("{}: {}".format(e, executable))
+        else:
+            raise
+
+# unhandled arguments so bail
+else:
+    exitWithError("Invalid arguments given: {}\n".format(" ".join(sys.argv)) +
+    "Usage: python runCertTestsLocally.py openfast_executable")
 
 # if the local output directory already exists, bail for two reasons
 # 1. don't silenty overwrite previous outputs
