7de88d45d80663e341eae626ccd7d32149399b06
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-17 12:50:16 -0600

    remove built flag and add architecture type flag to the python script for compiling ServoDyn controllers

--- inputs/compileDISCON.py
+++ inputs/compileDISCON.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 def exitWithInputError():
-  print("ERROR: Invalid arguments.\nUSAGE: python compileDISCON.py compiler_type[gnu,intel] build_type[debug,release]")
+  print("ERROR: Invalid arguments.\nUSAGE: python compileDISCON.py compiler_type[gnu,intel] arch_type[32/64]")
   sys.exit(1)
 
 if len(sys.argv) != 3:
@@ -20,10 +20,9 @@ if compilerInput.lower() != "intel" and compilerInput.lower() != "gnu":
   exitWithInputError()
 compiler = compilerInput.lower()
 
-buildInput = sys.argv[2]
-if buildInput.upper() != "DEBUG" and buildInput.upper() != "RELEASE":
+archType = sys.argv[2]
+if archType != "32" and archType != "64":
   exitWithInputError()
-build = buildInput.upper()
 
 makebase = os.path.join("makefile_DISCON")
 for ext in ["", "_ITI", "_OC3"]:
@@ -32,6 +31,6 @@ for ext in ["", "_ITI", "_OC3"]:
       print("ERROR: The DISCON makefile, {}, does not exist.".format(makefile))
       sys.exit(1)
 
-    make_command = "make -f {} COMPILER={} BUILD={} > {}.log".format(makefile, compiler, build, makefile)
+    make_command = "make -f {} COMPILER={} BUILD=RELEASE BITS={} > {}.log".format(makefile, compiler, archType, makefile)
     return_code = subprocess.call(make_command, shell=True)
     print("-- `make` for DISCON{} finished with return code: {}".format(ext, return_code))
