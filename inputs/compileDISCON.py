ec4d3618bae5f62b608873d3813ece751ade7e76
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-31 14:23:33 -0600

    rework ServoDyn controller compilation script to run cmake; make; make install with individual cmake projects

--- inputs/compileDISCON.py
+++ inputs/compileDISCON.py
@@ -19,10 +19,14 @@ import os
 import subprocess
 
 def exitWithInputError():
-  print("ERROR: Invalid arguments given: {}.\nUSAGE: python3 compileDISCON.py compiler_type[gnu,intel] arch_type[32/64]".format(sys.argv))
+  print("ERROR: Invalid arguments given: {}.\nUSAGE: python3 compileDISCON.py".format(sys.argv))
   sys.exit(1)
 
-if len(sys.argv) != 3:
+def exitWithReturnCode(return_code):
+  print("-- Finished with return code: {}".format(return_code))
+  sys.exit(return_code)
+
+if len(sys.argv) != 1:
   exitWithInputError()
 
 # parse the directory of this script if used in the call
@@ -30,23 +34,33 @@ if len(sys.argv) != 3:
 if os.path.sep in sys.argv[0]:
   splitpath = sys.argv[0].split(os.path.sep)
   os.chdir(os.path.sep.join(splitpath[0:len(splitpath)-1]))
+parentDir = os.getcwd()
 
-compilerInput = sys.argv[1]
-if compilerInput.lower() != "intel" and compilerInput.lower() != "gnu":
-  exitWithInputError()
-compiler = compilerInput.lower()
-
-archType = sys.argv[2]
-if archType != "32" and archType != "64":
-  exitWithInputError()
-
-makebase = os.path.join("makefile_DISCON")
+sourcebase = os.path.join("5MW_Baseline", "ServoData", "DISCON")
 for ext in ["", "_ITI", "_OC3"]:
-    makefile = makebase+ext
-    if not os.path.isfile(makefile):
-      print("ERROR: The DISCON makefile, {}, does not exist.".format(makefile))
-      sys.exit(1)
 
-    make_command = "make -f {} COMPILER={} BUILD=RELEASE BITS={} > {}.log".format(makefile, compiler, archType, makefile)
-    return_code = subprocess.call(make_command, shell=True)
-    print("-- `make` for DISCON{} finished with return code: {}".format(ext, return_code))
+  sourcedir = os.path.join(parentDir, sourcebase + ext)
+  if not os.path.isdir(sourcedir):
+    print("ERROR: The DISCON source directory, {}, does not exist.".format(sourcedir))
+    sys.exit(1)
+
+  os.chdir(sourcedir)
+
+  cmake_command = "cmake ."
+  return_code = subprocess.call(cmake_command, shell=True)
+  if return_code != 0:
+    print("-- Finished DISCON{} with return code: {}".format(ext, return_code))
+    continue
+
+  make_command = "make"
+  return_code = subprocess.call(make_command, shell=True)
+  if return_code != 0:
+    print("-- Finished DISCON{} with return code: {}".format(ext, return_code))
+    continue
+
+  install_command = "make install"
+  return_code = subprocess.call(install_command, shell=True)
+
+  os.chdir(parentDir)
+
+  print("-- Finished DISCON{} with return code: {}".format(ext, return_code))
