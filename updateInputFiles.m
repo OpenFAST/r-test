b028e4cb6ec57d2b084b6522e39c75a1da4387fb
Bonnie Jonkman <bonnie.jonkman@envision-energy.com>
2018-05-18 09:59:50 -0600

    added matlab script to convert input files
    using this script requires the OpenFAST/matlab-toolbox

---
+++ updateInputFiles.m
@@ -0,0 +1,27 @@
+%%
+caseFile = './glue-codes/fast/CaseList.md';
+[pathstr] = fileparts(caseFile);
+
+fid = fopen(caseFile);
+caseNames = textscan(fid,'%s');
+caseNames = caseNames{1};
+fclose(fid);
+
+for i= 1:length(caseNames)
+    casePath = [ pathstr filesep caseNames{i} ];
+    ConvertFAST8_16to17( [casePath filesep caseNames{i} '.fst'], casePath );
+end
+
+%%
+caseFile = './modules-local/beamdyn/CaseList.md';
+[pathstr] = fileparts(caseFile);
+
+fid = fopen(caseFile);
+caseNames = textscan(fid,'%s');
+caseNames = caseNames{1};
+fclose(fid);
+
+for i= 1:length(caseNames)
+    casePath = [ pathstr filesep caseNames{i} ];
+    ConvertBeamDynDriver( [casePath filesep 'bd_driver.fst'], casePath );  
+end
