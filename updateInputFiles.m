740c5d90db68043c778ffb18ff0df426a82ad8aa
Bonnie Jonkman <bonnie.jonkman@envision-energy.com>
2018-05-23 22:33:20 -0600

    added code to convert example input files in OpenFAST documentation

--- updateInputFiles.m
+++ updateInputFiles.m
@@ -1,4 +1,4 @@
-%%
+%% example files in the OpenFAST glue-code regression tests:
 caseFile = './glue-codes/fast/CaseList.md';
 [pathstr] = fileparts(caseFile);
 
@@ -12,7 +12,8 @@ for i= 1:length(caseNames)
     ConvertFAST8_16to17( [casePath filesep caseNames{i} '.fst'], casePath );
 end
 
-%%
+
+%% example files in the OpenFAST BeamDyn regression tests:
 caseFile = './modules-local/beamdyn/CaseList.md';
 [pathstr] = fileparts(caseFile);
 
@@ -24,4 +25,21 @@ fclose(fid);
 for i= 1:length(caseNames)
     casePath = [ pathstr filesep caseNames{i} ];
     ConvertBeamDynDriver( [casePath filesep 'bd_driver.inp'], casePath );  
+end
+
+
+%% example files in the OpenFAST BeamDyn documentation:
+docsPath = '../../docs/source/user/beamdyn/examples/';
+caseNames = strcat(docsPath, {'bd_driver_dynamic_nrel_5mw.inp', 'bd_driver_static_nrel_5mw.inp'} );
+
+for i= 1:length(caseNames)
+    ConvertBeamDynDriver( caseNames{i}, docsPath );  
+end
+
+%% example files in the OpenFAST AeroDyn documentation:
+docsPath = '../../docs/source/user/aerodyn/examples/';
+caseNames = strcat(docsPath, {'ad_driver_example.inp'} );
+
+for i= 1:length(caseNames)
+    ConvertAeroDynDriver( caseNames{i}, docsPath );  
 end
