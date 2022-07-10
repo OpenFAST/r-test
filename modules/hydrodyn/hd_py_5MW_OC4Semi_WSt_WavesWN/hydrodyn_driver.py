828c075159668959dad519cc75a9418ee622b7ce
Rafael M Mudafort <rafmudaf@gmail.com>
2022-07-10 12:30:58 -0500

    HD-Py: Fix module name in output

--- modules/hydrodyn/hd_py_5MW_OC4Semi_WSt_WavesWN/hydrodyn_driver.py
+++ modules/hydrodyn/hd_py_5MW_OC4Semi_WSt_WavesWN/hydrodyn_driver.py
@@ -70,12 +70,12 @@ elif sys.platform == "win32":
     if not os.path.isfile(library_path) and not sys.maxsize > 2**32:        # Try VS build location otherwise
         library_path = os.path.sep.join(["..", "..", "..", "..", "..", "build", "bin", "HydroDyn_c_lib_Win32.dll"]) # VS build install location
         if not os.path.isfile(library_path):
-            print(f"Python is 32 bit and cannot find 32 bit InflowWind DLL expected at: {library_path}")
+            print(f"Python is 32 bit and cannot find 32 bit HydroDyn DLL expected at: {library_path}")
             exit(1)
     if not os.path.isfile(library_path) and sys.maxsize > 2**32:        # Try VS build location otherwise
         library_path = os.path.sep.join(["..", "..", "..", "..", "..", "build", "bin", "HydroDyn_c_lib_x64.dll"]) # VS build install location
         if not os.path.isfile(library_path):
-            print(f"Python is 64 bit and cannot find 64 bit InflowWind DLL expected at: {library_path}")
+            print(f"Python is 64 bit and cannot find 64 bit HydroDyn DLL expected at: {library_path}")
             exit(1)
 
 
